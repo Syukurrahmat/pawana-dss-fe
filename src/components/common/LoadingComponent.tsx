b190e3f2e5c3c07569619a082d53c9473206eb2d
Syukurrahmat <syukurtullah@gmail.com>
2024-09-11 12:15:31 +0700

    move loading component

---
+++ src/components/common/LoadingComponent.tsx
@@ -0,0 +1,33 @@
+import {
+	Heading,
+	Spinner,
+	Stack,
+	Text,
+	VStack
+} from '@chakra-ui/react';
+
+export default function LoadingComponent() {
+	return (
+		<Stack
+			w="full"
+			direction={{ base: 'column', sm: 'row' }}
+			justifyContent="center"
+			align="center"
+			spacing="6"
+			pb="40"
+		>
+			<Spinner
+				thickness="5px"
+				speed="0.65s"
+				emptyColor="gray.200"
+				color="blue.500"
+				boxSize="58px"
+			/>
+			<VStack spacing='0' align={{base : 'center', sm : 'start'}} color="gray.500" >
+				<Heading size="lg">Mengambil Data</Heading>
+				<Text>Harap tunggu sebentar </Text>
+			</VStack>
+		</Stack>
+	);
+}
+//
