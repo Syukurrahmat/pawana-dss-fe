b06daaff44ae50df3ae11c4551958119c70c26c5
Syukurrahmat <syukurtullah@gmail.com>
2024-09-12 22:12:39 +0700

    integrate API URL with env file

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,5 @@
-const mode : string = (import.meta as any).env.MODE
-
-export const API_URL = mode === "development" ? "http://localhost:3000/api" : "/api"
-export const HOST_URL = mode === "development" ? "http://localhost:3000" : ""
-export const CENTER_OF_MAP  = [-7.519794, 110.082142] as [number, number]
-export const SUBS_LIMIT  = 5
+//@ts-ignore
+export const SERVER_URL = import.meta.env.DEV ? import.meta.env.VITE_SERVER_URL : "";
+export const API_URL = SERVER_URL + "/api"
+export const CENTER_OF_MAP = [-7.519794, 110.082142] as [number, number]
+export const SUBS_LIMIT = 5
