bbd87622aba9a799969e738820a5ccd8d29d6fed
Syukurrahmat <syukurtullah@gmail.com>
2024-09-13 21:56:31 +0700

    Add error handling to fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -3,19 +3,28 @@ import axios from "axios";
 import qs from "qs";
 
 export const fetcher = async <T = any>(url: string, options?: RequestInit) => {
+    try {
+        const response = await fetch(API_URL + url, {
+            credentials: 'include',
+            ...options
+        });
+        
+        if(response.redirected){
+            window.location.href = response.url;
+        }
 
-    return await fetch(API_URL + url, options)
-        .then(e => e.json())
-        .then((e: APIResponse<T>) => {
-            if (e.error) throw Error(e.error)
-                
-            return e.data
-        })
-        .catch((e)=>{
-            throw Error(e)
-        })
-}
+        if (!response.ok) throw new Error(response.statusText);
 
+        const result: APIResponse<T> = await response.json();
+        if (result.error) throw new Error(result.error);
+
+        return result.data;
+
+    } catch (error) {
+        console.error("Fetch error:", error);
+        throw new Error(error instanceof Error ? error.message : 'An unknown error occurred');
+    }
+};
 
 export function UrlWithQuery(baseUrl: string, additionalParams: Record<string, any>): string {
     const [urlPath, existingQuery] = baseUrl.split('?');
@@ -25,11 +34,9 @@ export function UrlWithQuery(baseUrl: string, additionalParams: Record<string, a
     return `${urlPath}?${queryString}`;
 }
 
-
-
 export const myAxios = axios.create({
     baseURL: API_URL,
-
+    withCredentials: true,
     transformResponse: (e) => {
         const response = JSON.parse(e)
         return response.statusCode == 200 ? response.data : response
